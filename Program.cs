fb6eaf4140aab2d098f4fbf89cff71956ed5d8ca
GodWasaProgrammer <bwnils@gmail.com>
2023-01-23 20:33:59 +0100

    made my quizobject

--- Program.cs
+++ Program.cs
@@ -2,51 +2,16 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var myQuestions  = new List<string>();
+            var newQuiz = new Quiz();
 
-            var myAnswers = new List<string>();
+
             string correctAnswer;
             Console.WriteLine("Input your new question.");
-            myQuestions.Add(Console.ReadLine());
-            File.AppendAllLines("Questions.txt", myQuestions);
+            newQuiz.quizQuestion = Console.ReadLine();
 
-            Console.WriteLine("Input your different answers");
-            while(myAnswers.Count < 3) 
-            { 
-                myAnswers.Add(Console.ReadLine());
-            }
-            do
-            {
-                Console.WriteLine("Type the correct answer:");
-                correctAnswer = Console.ReadLine();
-            } while (!myAnswers.Contains(correctAnswer));
-
-            File.AppendAllLines("Answers.txt", myAnswers);
             
-            Console.WriteLine(myQuestions[0]);
-
-            Console.WriteLine("Pick your answer:");
-            foreach (string answer in myAnswers)
-            {
-                Console.WriteLine(answer);
-            }
-
-            string myGuess = Console.ReadLine();
-
-            int myGuessAsInt = myAnswers.IndexOf(myGuess);
-
-            if (myGuess == myAnswers[myGuessAsInt])
-            {
-                Console.WriteLine("That is Correct");
-            }
-
-            else
-            {
-                Console.WriteLine("That is not correct");
-            }
-
         }
     }
 }
---
+++ Quiz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizMaker_RM
+{
+    public class Quiz
+    {
+        public string quizQuestion;
+        public string optionAnswer1;
+        public string optionAnswer2;
+        public string optionAnswer3;
+        public string correctAnswer;
+        
+    }
+
+
+}
